6e6f275781d7748df96d37df50905a3c09383fe6
Axel Mendoza <axel.mendoza@epita.fr>
2025-08-13 16:16:02 +0200

    add all GCP apis for WIF

--- modules/apis/main.tf
+++ modules/apis/main.tf
@@ -1,7 +1,19 @@
-resource "google_project_service" "compute" {
-  service = "compute.googleapis.com"
+locals {
+  apis = [
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "iamcredentials.googleapis.com",
+    "sts.googleapis.com",
+    "compute.googleapis.com",
+    "servicenetworking.googleapis.com"
+  ]
 }
 
-resource "google_project_service" "servicenetworking" {
-  service = "servicenetworking.googleapis.com"
+resource "google_project_service" "wif_required_apis" {
+  for_each = toset(local.apis)
+
+  service = each.value
+
+  disable_on_destroy         = false
+  disable_dependent_services = false
 }
