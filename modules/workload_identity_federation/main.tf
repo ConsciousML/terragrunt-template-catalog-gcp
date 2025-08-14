fed79ea8e8aaf665727e018d954a889f304294c0
Axel Mendoza <axel.mendoza@epita.fr>
2025-08-14 18:15:05 +0200

    add workload_identity_federation tf module

---
+++ modules/workload_identity_federation/main.tf
@@ -0,0 +1,45 @@
+# Create the container responsible to manage non-GCP entities
+resource "google_iam_workload_identity_pool" "github_pool" {
+  project                   = var.project
+  workload_identity_pool_id = var.workload_identity_pool_id
+  display_name              = var.display_name
+  description               = var.description
+}
+
+# Configure the WIP
+resource "google_iam_workload_identity_pool_provider" "github" {
+  project = var.project
+
+  # Gives access to the GitHub provider
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-provider"
+
+  # Maps GitHub OIDC token claims to GCP attributes
+  attribute_mapping = {
+    "attribute.aud"              = "assertion.aud"
+    "google.subject"             = "assertion.sub"
+    "attribute.sub"              = "assertion.sub"
+    "attribute.actor"            = "assertion.actor"
+    "attribute.repository"       = "assertion.repository"
+    "attribute.repository_owner" = "assertion.repository_owner"
+    "attribute.ref"              = "assertion.ref"
+  }
+
+  # Declares to use the OIDC method and points to the GitHub Actions token
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+# Create a service account dedicated for GitHub Workflows
+resource "google_service_account" "github_actions" {
+  account_id   = var.service_account_id
+  display_name = var.service_account_display_name
+  description  = var.service_account_description
+}
+
+resource "google_service_account_iam_member" "workload_identity_user" {
+  service_account_id = "projects/${var.project}/serviceAccounts/${google_service_account.github_actions.email}"
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${var.github_username}/${var.github_repo_name}"
+}
---
+++ modules/workload_identity_federation/variables.tf
@@ -0,0 +1,44 @@
+variable "project" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "workload_identity_pool_id" {
+  description = "ID of the workload identity pool"
+  type        = string
+}
+
+variable "display_name" {
+  description = "Display name for the workload identity pool"
+  type        = string
+}
+
+variable "description" {
+  description = "Description for the workload identity pool"
+  type        = string
+}
+
+variable "service_account_id" {
+  description = "ID of the service account"
+  type        = string
+}
+
+variable "service_account_display_name" {
+  description = "Display name for the service account"
+  type        = string
+}
+
+variable "service_account_description" {
+  description = "Description for the service account"
+  type        = string
+}
+
+variable "github_username" {
+  description = "GitHub username"
+  type        = string
+}
+
+variable "github_repo_name" {
+  description = "GitHub repository name"
+  type        = string
+}
