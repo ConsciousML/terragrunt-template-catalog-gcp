53608071e4badd6f7056f10a7215333f0bd9b368
Axel Mendoza <axel.mendoza@epita.fr>
2025-08-20 17:22:23 +0200

    add go test for vpc_gce stack

---
+++ tests/vpc_db_stack_test.go
@@ -0,0 +1,49 @@
+package tests
+
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLocalStack(t *testing.T) {
+    t.Parallel()
+
+    stackDir := "../examples/stacks/vpc_gce/local"
+
+    // Ensure destroy runs at the end, even if the test fails
+    t.Cleanup(func() {
+        t.Log("Running cleanup: terragrunt destroy...")
+        cmdDestroy := exec.Command("terragrunt", "--non-interactive", "stack", "run", "destroy")
+        cmdDestroy.Dir = stackDir
+        out, err := cmdDestroy.CombinedOutput()
+
+        t.Logf("Destroy output:\n%s", string(out))
+        require.NoError(t, err, "stack run destroy failed: %s", string(out))
+    })
+    
+    // Generate
+    t.Log("Running: terragrunt stack generate...")
+    cmdGenerate := exec.Command("terragrunt", "stack", "generate")
+    cmdGenerate.Dir = stackDir
+    out, err := cmdGenerate.CombinedOutput()
+
+    t.Logf("Generate output:\n%s", string(out))
+    require.NoError(t, err, "stack generate failed: %s", string(out))
+
+    // Apply
+    t.Log("Running: terragrunt stack run apply...")
+    cmdApply := exec.Command("terragrunt", "--non-interactive", "stack", "run", "apply", "--backend-bootstrap")
+    cmdApply.Dir = stackDir
+    out, err = cmdApply.CombinedOutput()
+
+    t.Logf("Apply output:\n%s", string(out))
+    require.NoError(t, err, "stack run apply failed: %s", string(out))
+
+    // Check that all three modules were applied successfully
+    require.Contains(t, string(out), "vpc] tofu: Apply complete!")
+    require.Contains(t, string(out), "apis] tofu: Apply complete!")
+    require.Contains(t, string(out), "gce] tofu: Apply complete!")
+
+}
